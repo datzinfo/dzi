9165266c71ff95ed924a3ece3b32420711603ba1
dzi <kh_fung@hotmail.com>
2016-07-07 22:11:52 -0400

    Initial version

---
+++ public/assets/js/messages.js
@@ -0,0 +1,139 @@
+var strings = {
+		//Index--Slider Text//
+		index_slider1_title1: 'Creative Design & Best Feature',
+		index_slider1_title2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since',
+		index_slider2_title1: 'We are Best Team & Support you always',
+		index_slider2_title2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since',
+		
+		//Index--Feature Text//
+		index_feature_title: 'Features',
+		index_feature_description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+		
+		//Index--Box Text//
+		index_box1_title: 'Creative Design',
+		index_box1_desc: 'desc',
+		index_box2_title: 'Responsive Layouts',
+		index_box2_desc: 'desc',
+		index_box3_title: 'Great Features',
+		index_box3_desc: 'desc',
+		index_box4_title: 'Multiple Options',
+		index_box4_desc: 'desc',
+		index_box5_title: 'Quality Code',
+		index_box5_desc: 'desc',
+		index_box6_title: 'Awesome Support',
+		index_box6_desc: 'desc',
+		
+		//About Us--Banner Text//
+		aboutus_banner_title: 'About us',
+		aboutus_banner_desc: 'desc',
+		
+		//About Us--Title Text//
+		aboutus_main_title: 'Who we are',
+		aboutus_main_desc: 'desc',
+		aboutus_skills_title: 'Our Skills',
+		about_skills_desc: 'desc',
+		
+		//About Us--Skill Text//
+		//Add note about percentage, to update skill level//
+		aboutus_skill1: 'skill 1',
+		aboutus_skill2: 'skill 2',
+		aboutus_skill3: 'skill 3',
+		aboutus_skill4: 'skill 4',
+		aboutus_skill5: 'skill 5',
+		aboutus_skill6: 'skill 6',
+		
+		
+		//About Us--Why Choose Us? Text//
+		aboutus_why_title: 'Why Choose Us?',
+		aboutus_why_reason1_header: 'Header',
+		aboutus_why_reason1_desc: 'Desc',
+		aboutus_why_reason2_header: 'Header',
+		aboutus_why_reason2_desc: 'Desc',
+		aboutus_why_reason3_header: 'Header',
+		aboutus_why_reason3_desc: 'Desc',
+		
+		//Services--Banner Text//
+		services_banner_title: 'Service',
+		services_banner_desc: 'desc',
+		
+		//Services--Title Text//
+		services_main_title: 'Our Services',
+		services_main_desc: 'desc',
+		
+		//Services--Box Text//
+		services_box1_title: 'title',
+		services_box1_desc: 'desc',
+		services_box2_title: 'title',
+		services_box2_desc: 'desc',
+		services_box3_title: 'title',
+		services_box3_desc: 'desc',
+		services_box4_title: 'title',
+		services_box4_desc: 'desc',
+		services_box5_title: 'title',
+		services_box5_desc: 'desc',
+		services_box6_title: 'title',
+		services_box6_desc: 'desc',
+		
+		//Blog--Banner Text//
+		blog_banner_title: 'title',
+		blog_banner_desc: 'desc',
+		
+		//Blog--Categories Text//
+		blog_categories_title: 'Catagories',
+		blog_categories_catagory1: 'Multimedia',
+		blog_categories_catagory2: 'Business Intelligence',
+		blog_categories_catagory3: 'Application Development',
+		blog_categories_catagory4: 'Catagory 4',
+		blog_catagories_catagory5: 'Catagory 5',
+		
+		//Blog--Text Widget Text//
+		blog_widget_title: 'Title',
+		blog_widget_desc: 'Desc',
+		
+		//Blog--Tags Text//
+		blog_tags_title: 'Title',
+		blog_tags_tag1: 'tag1',
+		blog_tags_tag2: 'tag2',
+		blog_tags_tag3: 'tag3',
+		blog_tags_tag4: 'tag4',
+		blog_tags_tag5: 'tag5',
+		blog_tags_tag6: 'tag6',
+		blog_tags_tag7: 'tag7',
+		blog_tags_tag8: 'tag8',
+		blog_tags_tag9: 'tag9',
+		blog_tags_tag10: 'tag10',
+		
+		//Blog--Archive Text//
+		blog_archive_title: 'Archive',
+		blog_archive_date1: 'June 2016',
+		blog_archive_num1: '#',
+		blog_arvice_date2: 'July 2016',
+		blog_archive_num2: '#',
+		
+		//Blog--Link Text//
+		blog_link_title: 'Important Links',
+		blog_link_link1: 'link 1',
+		blog_link_link2: 'link 2',
+		blog_link_link3: 'link 3',
+		blog_link_link4: 'link 4',
+		
+		//Contact--Banner Text//
+		contact_banner_title: 'Contact',
+		contact_banner_desc: 'desc',
+		
+		//Contact--Title Text
+		contact_main_questions_title: 'Have any questions?',
+		contact_main_questions_desc: 'desc',
+		contact_info_title: 'Contact Info'
+
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+}
