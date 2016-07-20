8ea5c4c0e7436339081e6d4cdb30c653eafe5e58
Kaitlin <Kaitlin@192.168.1.9>
2016-07-19 20:33:45 -0400

    july 19 2016

--- public/common/ngwig/custom.js
+++ public/common/ngwig/custom.js
@@ -1,9 +1,20 @@
 var VideoCtl = function($scope) {
 	var ctrl = this;
-	ctrl.videoUrl = '';
+	ctrl.videoUrl = '';	
 	
-    ctrl.onclick = function() {   	
-    	pasteHtmlAtCaret(ctrl.videoUrl);
+    ctrl.onclick = function() { 
+    	if (ctrl.videoUrl){
+    		var spot =  ctrl.videoUrl.replace('watch?v=', 'embed/');
+    		var videolink = "<iframe width='420' height='315'"
+				 +"src='" + spot + "'>"
+				 +"</iframe>";
+    		pasteHtmlAtCaret(videolink);
+    	}
+    	else{
+    		alert ("No URL pasted")
+    	}
+    	
+    
     }
  
 };
