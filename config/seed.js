c6aff2daec4e8bf249f7a5f836d41321e7509679
dzi <kh_fung@hotmail.com>
2016-09-05 12:25:19 -0400

    seed update

--- config/seed.js
+++ config/seed.js
@@ -16,47 +16,59 @@ module.exports = function(models) {
 	  });
 
 	models.category
-	  .findOrCreate({where: {type: 'Multimedia'}, defaults: {desc: 'Multimedia desc', blogId: '1'}})
+	  .findOrCreate({where: {type: 'Tech Tips'}, defaults: {desc: 'Tech how-tos', blogId: '1'}})
 	  .spread(function(category, created) {
 		  if (created) {
 		    console.log(category.get({
 		      plain: true
 		    }));
-		    console.log('Created category Multimedia: ' + created);
+		    console.log('Created category Tech Tips: ' + created);
 		  }
 	  });
 
 	models.category
-	  .findOrCreate({where: {type: 'Business Intelligence'}, defaults: {desc: 'Business Intelligence desc', blogId: '1'}})
+	  .findOrCreate({where: {type: 'Business Insights'}, defaults: {desc: 'Anything relating to the business', blogId: '1'}})
 	  .spread(function(category, created) {
 		  if (created) {
 		    console.log(category.get({
 		      plain: true
 		    }));
-		    console.log('Created category Business Intelligence: ' + created);
+		    console.log('Created category Business: ' + created);
 		  }
 	  });
 
 	models.category
-	  .findOrCreate({where: {type: 'Application Development'}, defaults: {desc: 'Application Development desc', blogId: '1'}})
+	  .findOrCreate({where: {type: 'News & Announcements'}, defaults: {desc: 'News and annoucements', blogId: '1'}})
 	  .spread(function(category, created) {
 		  if (created) {
 		    console.log(category.get({
 		      plain: true
 		    }));
-		    console.log('Created category Application Development: ' + created);
+		    console.log('Created category News & Announcements: ' + created);
 		  }
 	  });
 
 	var pw = secure.hash('dziadmin');
 	models.user
-	  .findOrCreate({where: {username: 'dziadmin'}, defaults: {password: pw, email: "michaelfung@datzinfo.com", isAdmin: true, active: true}})
+	  .findOrCreate({where: {username: 'mike'}, defaults: {password: pw, email: "michaelfung@datzinfo.com", isAdmin: true, active: true}})
 	  .spread(function(user, created) {
 		  if (created) {
 		    console.log(user.get({
 		      plain: true
 		    }));
-		    console.log('Created admin user: ' + created);
+		    console.log('Created admin user mike: ' + created);
+		  }
+	  });
+	
+	var pw = secure.hash('dziadmin');
+	models.user
+	  .findOrCreate({where: {username: 'khuan'}, defaults: {password: pw, email: "kkh@datzinfo.com", isAdmin: true, active: true}})
+	  .spread(function(user, created) {
+		  if (created) {
+		    console.log(user.get({
+		      plain: true
+		    }));
+		    console.log('Created admin user khuan: ' + created);
 		  }
 	  });
 };
